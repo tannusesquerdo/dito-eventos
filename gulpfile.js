23727c7fe366f595ffe4dbbd44674dee63fd9dcf
alistairtweedie <info@alistairtweedie.com>
2015-09-29 13:58:13 +0100

    refined watch task

--- gulpfile.js
+++ gulpfile.js
@@ -23,6 +23,8 @@ gulp.task('serve', ['bs-reload', 'browser-sync'], function(){
   gulp.watch('./public/css/site.css', ['bs-reload']);
   gulp.watch('./src/templates/**/*.html', ['build-html']);
   gulp.watch('./public/*.html', ['bs-reload']);
+  gulp.watch('./src/js/**/*.js', ['build-scripts']);
+  gulp.watch('./public/js/*.js', ['bs-reload']);
 });
 
 
