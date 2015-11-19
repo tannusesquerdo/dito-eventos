846269644330984597b21ffe3bf473f6d3f27562
alistairtweedie <info@alistairtweedie.com>
2015-11-19 23:57:09 +0000

    middleware added to gzip when serving files

--- tasks/browser-sync.js
+++ tasks/browser-sync.js
@@ -1,11 +1,13 @@
 var gulp = require('gulp'),
 	browserSync = require('browser-sync'),
+    compress = require('compression');
 	browserReload = browserSync.reload;
 
 gulp.task('browser-sync', function() {
     browserSync.init(null, {
         server: {
-            baseDir: "./public/"
+            baseDir: "./public/",
+            middleware: [compress()]
         },
         port: 3000,
         tunnel: "sqone"
