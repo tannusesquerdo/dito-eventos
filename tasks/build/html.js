1e900fc9ebbfc04f5e7bc29991977219f0069614
alistairtweedie <info@alistairtweedie.com>
2016-04-11 19:51:23 +0100

    latest working build

--- tasks/build/html.js
+++ tasks/build/html.js
@@ -2,11 +2,42 @@ var gulp = require('gulp'),
     debug = require('gulp-debug'),
     util = require("gulp-util"),
     browserSync = require('browser-sync'),
-    nunjucksRender = require('gulp-nunjucks-render'),
+    // nunjucksRender = require('gulp-nunjucks-render'),
+    twig = require('gulp-twig'),
     data = require('gulp-data'),
     htmlmin = require('gulp-htmlmin'),
     browserReload = browserSync.reload;
 
+
+// gulp.task('build-html', function() {
+
+//     var minOps = {
+//         collapseWhitespace: true,
+//         minifyJS: true,
+//         minifyCSS: true,
+//         removeComments: true
+//     };
+
+//     nunjucksRender.nunjucks.configure(['./src/templates/'], {watch: false});
+
+
+//     return gulp.src('./src/pages/**/*.+(html|nunjucks)')
+
+
+//     .pipe(data(function() {
+
+//         return require('../../src/site.json');
+
+//     }))
+
+//     .pipe(nunjucksRender())
+
+//     .pipe(!!util.env.prod ? htmlmin(minOps) : util.noop())
+
+//     .pipe(gulp.dest('./public/'))
+//     .pipe(browserSync.reload({stream:true}));
+
+// });
 
 gulp.task('build-html', function() {
 
@@ -17,19 +48,15 @@ gulp.task('build-html', function() {
         removeComments: true
     };
 
-    nunjucksRender.nunjucks.configure(['./src/templates/'], {watch: false});
+    return gulp.src(['./src/templates/base.twig', './src/pages/**/*.twig'])
 
-
-    return gulp.src('./src/pages/**/*.+(html|nunjucks)')
-
-
-    .pipe(data(function() {
+    .pipe(data(function(file) {
 
         return require('../../src/site.json');
 
     }))
 
-    .pipe(nunjucksRender())
+    .pipe(twig())
 
     .pipe(!!util.env.prod ? htmlmin(minOps) : util.noop())
 
