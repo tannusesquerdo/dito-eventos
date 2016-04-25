e58161cd39e1e45b986550854e63ea08974b9755
alistairtweedie <info@alistairtweedie.com>
2016-04-25 20:25:55 +0100

    gulp-image added

--- tasks/build/images.js
+++ tasks/build/images.js
@@ -1,12 +1,18 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+    image = require('gulp-image');
 
-
-var imageop = require('gulp-image-optimization');
-
-gulp.task('build-images', function(cb) {
-    gulp.src('src/images/*.+(png|jpg|gif|jpeg|svg)').pipe(imageop({
-        optimizationLevel: 5,
-        progressive: true,
-        interlaced: true
-    })).pipe(gulp.dest('public/images')).on('end', cb).on('error', cb);
+gulp.task('build-images', function () {
+  gulp.src('./src/images/*')
+    .pipe(image({
+      pngquant: true,
+      optipng: false,
+      zopflipng: true,
+      advpng: true,
+      jpegRecompress: false,
+      jpegoptim: true,
+      mozjpeg: true,
+      gifsicle: true,
+      svgo: true
+    }))
+    .pipe(gulp.dest('public/images'));
 });
