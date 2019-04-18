97d9c7accbd1fd210a101d3151e626bf30de4682
Tannus Esquerdo <tannus.esquerdo@dito.com.br>
2019-04-18 15:41:40 -0300

    add date to event

--- src/js/_form.js
+++ src/js/_form.js
@@ -17,12 +17,14 @@
   var form = document.getElementById('form-event');
   var success = document.getElementById('success');
   var form = document.getElementById('form-event');
-  var event = null
+  var event = undefined;
+  var dataEvento = undefined;
 
   $(document).ready(function(){
     $.getJSON('https://api.sheety.co/a2cbeff0-295a-49e3-87a3-d1824d75af43', function(data) {
       if(data.length && data[0].evento) {
-        event = data[0].evento;
+        event = data[0].event;
+        dataEvento = data[0].date;
         show(form);
       }
     })
@@ -44,7 +46,7 @@
     if(validate(email)) {
       var id = dito.generateID(email);
       var data = { id_type: 'id', id: id, email: email, name: name, data: data };
-      var eventData = { event: JSON.stringify({ action: 'participou-evento', data: { evento: event } })};
+      var eventData = { event: JSON.stringify({ action: 'participou-evento', data: { evento: event, data_evento: dataEvento } })};
   
       dito.Api.post('login', '/users/portal/'+id+'/signup', data, function(res) {
         if(res.data) {
